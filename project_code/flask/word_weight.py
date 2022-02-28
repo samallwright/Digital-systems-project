a2819c9f599551be8082858bb5ad46d7e1ad8449
samal <71259281+samallwright@users.noreply.github.com>
2022-02-28 13:09:22 +0000

    Add function to select sentences on prereq frequency

--- project_code/flask/word_weight.py
+++ project_code/flask/word_weight.py
@@ -6,11 +6,11 @@ from token_freq import basic_table
 input_text = get_text()
 
 
-def sentences(input_text: str):
+def sentences(input_text: str) -> any:
     return sent_tokenize(input_text)
 
 
-def weighting(sentence_tokens, frequency_table):
+def weighting(sentence_tokens, frequency_table) -> dict:
     weight = dict()
     for sentence in sentence_tokens:
         wordcount_without_stop = 0
@@ -25,4 +25,18 @@ def weighting(sentence_tokens, frequency_table):
     return weight
 
 
-print(weighting(sentences(input_text), basic_table(input_text)))
+def select_criteria_sentences(weighted_sentences: dict) -> list:
+    sentences = [
+        sentences
+        for sentences in weighted_sentences
+        if weighted_sentences[sentences] >= 1.5
+    ]
+    return sentences
+
+
+# print(weighting(sentences(input_text), basic_table(input_text)))
+# print(
+#     select_criteria_sentences(
+#         weighting(sentences(input_text), basic_table(input_text))
+#     )
+# )
