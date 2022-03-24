0655b0367a1e739f3898f4795e13c5b39b34487f
sam_allwright <sam_allwright1@gmail.com>
2022-03-24 14:20:00 +0000

    adds tf-idf combined score code

--- project_code/flask/token_frequencies.py
+++ project_code/flask/token_frequencies.py
@@ -4,8 +4,7 @@ from pickle import FRAME
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk import FreqDist, ngrams
-from text_functions import get_text
-from stop_word import stop_word_removal
+from text_functions import get_text, stop_word_removal
 
 
 def word_freq_table(input_text: str) -> dict:
@@ -18,12 +17,11 @@ def word_freq_table(input_text: str) -> dict:
             word_frequency[token] += 1
     word_sum = len(word_tokens)
     scored_words = {key: value / word_sum for (key, value) in word_frequency.items()}
-
     return scored_words
 
 
 def inverse_document_frequency(sentences, word_tokens) -> dict:
-    # idf = log(num_of_sentences/num_of_sentences_with_word)
+    """idf = log(num_of_sentences/num_of_sentences_with_word)"""
     sentence_count_for_word = dict()
     for sentence in sentences:
         for word in word_tokens:
@@ -32,24 +30,26 @@ def inverse_document_frequency(sentences, word_tokens) -> dict:
                     sentence_count_for_word[word] += 1
                 else:
                     sentence_count_for_word[word] = 1
-
-    # sentence_count_for_word[word] = log10(
-    #     len(sentences) / sentence_count_for_word[word]
-    # )
-    # print(sentence_count_for_word)
-    # sentence_count_for_word = {
-    #     key: log10(len(sentences) / sentence_count_for_word[value])
-    #     for (key, value) in sentence_count_for_word.items()
-    # }
     length = len(sentences)
-    dictfun = dict()
+    inverse_freq = dict()
     for word, frequency in sentence_count_for_word.items():
-        dictfun[word] = log10(length / frequency)
+        inverse_freq[word] = log10(length / frequency)
+    return inverse_freq
 
-    print(dictfun)
 
+def tf_idf_combine(term_frequencies, inverse_frequencies):
+    """A high weight in tf–idf is reached by a high term frequency (in the given document)
+    and a low document frequency of the term in the whole collection of documents;
+    the weights hence tend to filter out common terms.
+    tf-idf(t,d,D) = tf(t,d)*idf(t,D)"""
 
-# inverse_document_frequency(sent_tokenize(get_text()), word_tokenize(get_text()))
+    tf_idf = dict()
+    for key in term_frequencies.keys() & inverse_frequencies.keys():
+        # print(term_frequencies[key])
+        # print(inverse_frequencies[key])
+        tf_idf[key] = term_frequencies[key] * inverse_frequencies[key]
+
+    return tf_idf
 
 
 def token_dists(input_text: str) -> FreqDist:
@@ -84,18 +84,19 @@ def trigrams(tokens: FreqDist) -> dict:
 
 
 # input_text = get_text()
-# print(basic_table(input_text))
-# print(token_dists(input_text))
+# print(word_freq_table(input_text))
+# print(token_dists(input_text).items())
 # print(top_ten(token_dists(input_text)))
 # print(bottom_10(token_dists(input_text)))
 # print(single_occurrence(token_dists(input_text)))
 # print(bigrams(token_dists(input_text)))
 # print(trigrams(token_dists(input_text)).items())
-
-
-# stitching sentences with top ten word
-# also important infrquent words
-# words in title
-# how to select important infrequent
-# any sentence with top 3 words in, + any sentence with 10 least common words in, ordered
-# word pairs? (bigrams, trigrams, phrases)
+# print(inverse_document_frequency(sent_tokenize(input_text), word_tokenize(input_text)))
+# print(
+#     tf_idf(
+#         word_freq_table(input_text),
+#         inverse_document_frequency(
+#             sent_tokenize(input_text), word_tokenize(input_text)
+#         ),
+#     )
+# )
