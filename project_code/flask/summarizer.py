e042a0d47a388811da9496b5aa68cbf5a742e211
sam_allwright <sam_allwright1@gmail.com>
2022-04-14 22:51:36 +0100

    removal of sentences judged similar

--- project_code/flask/summarizer.py
+++ project_code/flask/summarizer.py
@@ -5,7 +5,12 @@ from token_frequencies import (
 )
 from word_weight import select_criteria_sentences, sentence_scoring
 from nltk.tokenize import word_tokenize, sent_tokenize
-from graph_model import graph_density, similarity_score, similarity_graph
+from graph_model import (
+    graph_density,
+    maximum_similarity,
+    similarity_graph,
+    weakest_links,
+)
 from text_functions import get_text
 
 
@@ -28,6 +33,17 @@ def summarizer(title, content, prerequisite):
     return summary
 
 
+def word_removal(similar_sentences, weighted_sentences):
+    list_words = list(weighted_sentences)
+    filtered_weighted_sentences = {
+        sentence: weight
+        for (sentence, weight) in weighted_sentences.items()
+        if list_words.index(sentence) not in similar_sentences
+    }
+    # print(len(weighted_sentences, len(filtered_weighted_sentences)))
+    return filtered_weighted_sentences
+
+
 input = get_text()
 wokens = word_tokenize(input)
 stokens = sent_tokenize(input)
@@ -36,4 +52,9 @@ density = graph_density(matrix, stokens)
 tf_idf = tf_idf_combine(
     word_freq_table(input), inverse_document_frequency(stokens, wokens)
 )
-similarity_score(density, sentence_scoring(stokens, tf_idf))
+weighted_sentences = sentence_scoring(stokens, tf_idf)
+most_similar = maximum_similarity(density)
+similar_sentences = weakest_links(most_similar)
+filtered_sentences = word_removal(similar_sentences, weighted_sentences)
+summary = select_criteria_sentences(filtered_sentences, 3)
+print(summary)
