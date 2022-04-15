00e5bad2d2e4de4b939e8f347492fe9bce44148a
sam_allwright <sam_allwright1@gmail.com>
2022-04-15 15:26:14 +0100

    implemented graph ranking to flask

--- project_code/flask/summarizer.py
+++ project_code/flask/summarizer.py
@@ -26,10 +26,20 @@ def summarizer(title, content, prerequisite):
     # sentence scoring returns sentences and their scores from tfidf
     # call graphing next to give score for removal
 
-    summary = select_criteria_sentences(
-        sentence_scoring(sentence_tokens, tf_idf),
-        prerequisite,
-    )
+    matrix = similarity_graph(sentence_tokens)
+    density = graph_density(matrix, sentence_tokens)
+    most_similar = maximum_similarity(density)
+    similar_sentences = weakest_links(most_similar)
+
+    weighted_sentences = sentence_scoring(sentence_tokens, tf_idf)
+    filtered_sentences = word_removal(similar_sentences, weighted_sentences)
+
+    summary = select_criteria_sentences(filtered_sentences, prerequisite)
+
+    # summary = select_criteria_sentences(
+    #     sentence_scoring(sentence_tokens, tf_idf),
+    #     prerequisite,
+    # )
     return summary
 
 
@@ -44,17 +54,17 @@ def word_removal(similar_sentences, weighted_sentences):
     return filtered_weighted_sentences
 
 
-input = get_text()
-wokens = word_tokenize(input)
-stokens = sent_tokenize(input)
-matrix = similarity_graph(stokens)
-density = graph_density(matrix, stokens)
-tf_idf = tf_idf_combine(
-    word_freq_table(input), inverse_document_frequency(stokens, wokens)
-)
-weighted_sentences = sentence_scoring(stokens, tf_idf)
-most_similar = maximum_similarity(density)
-similar_sentences = weakest_links(most_similar)
-filtered_sentences = word_removal(similar_sentences, weighted_sentences)
-summary = select_criteria_sentences(filtered_sentences, 3)
-print(summary)
+# input = get_text()
+# wokens = word_tokenize(input)
+# stokens = sent_tokenize(input)
+# matrix = similarity_graph(stokens)
+# density = graph_density(matrix, stokens)
+# tf_idf = tf_idf_combine(
+#     word_freq_table(input), inverse_document_frequency(stokens, wokens)
+# )
+# weighted_sentences = sentence_scoring(stokens, tf_idf)
+# most_similar = maximum_similarity(density)
+# similar_sentences = weakest_links(most_similar)
+# filtered_sentences = word_removal(similar_sentences, weighted_sentences)
+# summary = select_criteria_sentences(filtered_sentences, 3)
+# print(summary)
